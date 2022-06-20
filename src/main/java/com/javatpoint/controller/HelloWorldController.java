24c3531b8489331b760b1c86f635a150de8f714c
unknown <saranyaganesana@gmail.com>
2022-06-20 10:43:24 +0530

    Employee Get & Post

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,12 +1,9 @@
-package com.javatpoint.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "Hello javaTpoint";
-}
-}
+/*
+ * package com.javatpoint.controller; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * @RestController public class HelloWorldController {
+ * 
+ * @RequestMapping("/") public String hello() { return "Hello javaTpoint"; } }
+ */
